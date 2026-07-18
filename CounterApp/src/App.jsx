02eb111318f205c1781eac625c08de4361b7ee8b
vinayak833 <vinayakauradi83@gmail.com>
2026-07-18 23:07:27 +0530

    Added validation to prevent negative count

--- CounterApp/src/App.jsx
+++ CounterApp/src/App.jsx
@@ -1,31 +1,32 @@
 import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+
   const decrease = () => {
-  if (count > 0) {
-    setCount(count - 1);
-  }
-};
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
 
   return (
-    <div>
+    <div className="container">
       <h1>Counter App</h1>
 
       <h2>{count}</h2>
 
-     <button onClick={decrease}>
-        -
-      </button>
+      <div className="buttons">
+        <button onClick={decrease}>-</button>
 
-      <button onClick={() => setCount(0)}>
-         Reset
-      </button>
+        <button onClick={() => setCount(0)}>
+          Reset
+        </button>
 
-      <button onClick={() => setCount(count + 1)}>
-        +
-      </button>
-      
+        <button onClick={() => setCount(count + 1)}>
+          +
+        </button>
+      </div>
     </div>
   );
 }
