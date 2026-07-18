7d1e1979afd86bbe5d39b46db41faba716fefe83
vinayak833 <vinayaksa88@gmail.com>
2026-07-18 22:35:33 +0530

    Added increment and decrement functionality

--- CounterApp/src/App.jsx
+++ CounterApp/src/App.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>Counter App</h1>
-      <h2>0</h2>
 
-      <button>-</button>
+      <h2>{count}</h2>
+
+      <button onClick={() => setCount(count - 1)}>
+        -
+      </button>
+
       <button>Reset</button>
-      <button>+</button>
+
+      <button onClick={() => setCount(count + 1)}>
+        +
+      </button>
     </div>
   );
 }
